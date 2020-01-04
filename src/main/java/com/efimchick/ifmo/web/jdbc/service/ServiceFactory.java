73e56ba469f1467c328e38aa6c94c396f0dd9be7
ekaterinadidenko20 <ekaterinadidenko20@gmail.com>
2020-01-05 02:23:24 +0300

    -

--- src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
@@ -5,69 +5,34 @@ import com.efimchick.ifmo.web.jdbc.domain.Department;
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
 import com.efimchick.ifmo.web.jdbc.domain.FullName;
 import com.efimchick.ifmo.web.jdbc.domain.Position;
-
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-
+import java.util.*;
 
 public class ServiceFactory {
 
-    List<Employee> Eml;
-    List<Employee> EmlForChain;
-
-    {
-        try {
-            Eml = getEmployees();
-            EmlForChain = getEmployees();
-            fillManagers1lev();
-            fillManagersInfLev();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    List<Department> Deps;
-
-    {
-        try {
-            Deps = getDepartments();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Employee employeeRowMapper(ResultSet resultSet) throws SQLException {
+    private Employee employeeRowMapper(ResultSet resultSet, int lev) throws SQLException {
         try {
             Department department = null;
             if (resultSet.getString("DEPARTMENT") != null)
-                for (int i = 0; i < getDepartments().size(); i++) {
-                    if (BigInteger.valueOf(Long.parseLong(resultSet.getString("DEPARTMENT"))).equals(getDepartments().get(i).getId())) {
-                        department = getDepartments().get(i);
+                for (int i = 0; i < getAllDeps().size(); i++) {
+                    if (BigInteger.valueOf(Long.parseLong(resultSet.getString("DEPARTMENT"))).equals(getAllDeps().get(i).getId())) {
+                        department = getAllDeps().get(i);
                     }
                 }
             BigInteger manId = null;
             if (resultSet.getString("MANAGER") != null) {
                 manId = BigInteger.valueOf(Long.parseLong(resultSet.getString("MANAGER")));
             }
+            if (lev != 0)
+                manId = null;
             Employee manager = null;
             if (manId != null) {
-                manager = new Employee(
-                        manId,
-                        null,
-                        null,
-                        null,
-                        BigDecimal.ZERO,
-                        null,
-                        null
-                );
+                manager = getEmployeeById(manId, lev).get();
             }
             Employee e = new Employee(
                     BigInteger.valueOf(resultSet.getInt("ID")),
@@ -88,57 +53,40 @@ public class ServiceFactory {
         }
     }
 
-    void fillManagers1lev() {
-        for (int i = 0; i < Eml.size(); ++i) {
-            if (Eml.get(i).getManager() != null) {
-                for (int j = 0; j < Eml.size(); ++j) {
-                    if (Eml.get(j).getId().equals(Eml.get(i).getManager().getId())) {
-                        Employee cur = Eml.get(i);
-                        Employee cur2 = Eml.get(j);
-                        Eml.set(i, new Employee(
-                                cur.getId(),
-                                cur.getFullName(),
-                                cur.getPosition(),
-                                cur.getHired(),
-                                cur.getSalary(),
-                                new Employee(
-                                        cur2.getId(),
-                                        cur2.getFullName(),
-                                        cur2.getPosition(),
-                                        cur2.getHired(),
-                                        cur2.getSalary(),
-                                        null,
-                                        cur2.getDepartment()
-                                ),
-                                cur.getDepartment()
-                        ));
+    private Employee employeeRowMapperWithChain(ResultSet resultSet) throws SQLException {
+        try {
+            Department department = null;
+            if (resultSet.getString("DEPARTMENT") != null)
+                for (int i = 0; i < getAllDeps().size(); i++) {
+                    if (BigInteger.valueOf(Long.parseLong(resultSet.getString("DEPARTMENT"))).equals(getAllDeps().get(i).getId())) {
+                        department = getAllDeps().get(i);
                     }
                 }
+            BigInteger manId = null;
+            if (resultSet.getString("MANAGER") != null) {
+                manId = BigInteger.valueOf(Long.parseLong(resultSet.getString("MANAGER")));
             }
-        }
-    }
+            Employee manager = null;
+            if (manId != null) {
+                manager = getEmployeeByIdWithChain(manId).get();
+            }
 
-    void fillManagersInfLev() {
-        for (int k = 0; k < 5; ++k)
-            for (int i = 0; i < EmlForChain.size(); ++i) {
-                if (EmlForChain.get(i).getManager() != null) {
-                    for (int j = 0; j < EmlForChain.size(); ++j) {
-                        if (EmlForChain.get(j).getId().equals(EmlForChain.get(i).getManager().getId())) {
-                            Employee cur = EmlForChain.get(i);
-                            Employee cur2 = EmlForChain.get(j);
-                            EmlForChain.set(i, new Employee(
-                                    cur.getId(),
-                                    cur.getFullName(),
-                                    cur.getPosition(),
-                                    cur.getHired(),
-                                    cur.getSalary(),
-                                    cur2,
-                                    cur.getDepartment()
-                            ));
-                        }
-                    }
-                }
-            }
+            return new Employee(
+                    BigInteger.valueOf(resultSet.getInt("ID")),
+                    new FullName(
+                            resultSet.getString("FIRSTNAME"),
+                            resultSet.getString("LASTNAME"),
+                            resultSet.getString("MIDDLENAME")
+                    ),
+                    Position.valueOf(resultSet.getString("POSITION")),
+                    LocalDate.parse(resultSet.getString("HIREDATE")),
+                    BigDecimal.valueOf(resultSet.getInt("SALARY")),
+                    manager,
+                    department
+            );
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     private ResultSet getResultSet(String s) throws SQLException {
@@ -147,40 +95,131 @@ public class ServiceFactory {
         return connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery(s);
     }
 
-    private List<Employee> getEmployees() throws SQLException {
-        ResultSet resultSet = getResultSet("SELECT * FROM EMPLOYEE");
-        List<Employee> employees = new LinkedList<>();
-        while (resultSet.next()) {
-            Employee employee = employeeRowMapper(resultSet);
-            employees.add(employee);
-        }
-        return employees;
-    }
-
-    private List<Department> getDepartments() throws SQLException {
-        ResultSet resultSet = getResultSet("SELECT * FROM DEPARTMENT");
-        List<Department> departments = new LinkedList<>();
-        while (resultSet.next()) {
-            Department department = new Department(
-                    BigInteger.valueOf(resultSet.getInt("ID")),
-                    resultSet.getString("NAME"),
-                    resultSet.getString("LOCATION")
-            );
-            departments.add(department);
-        }
-        return departments;
-    }
 
     List<Employee> getPaging(List<Employee> list, Paging paging) {
         return list.subList(paging.itemPerPage * (paging.page - 1), Math.min(paging.itemPerPage * paging.page, list.size()));
     }
 
+    private Department departmentRowMapper(ResultSet resultSet) {
+        try {
+            return new Department(
+                    BigInteger.valueOf(resultSet.getInt("ID")),
+                    resultSet.getString("NAME"),
+                    resultSet.getString("LOCATION")
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public Optional<Employee> getEmployeeByIdWithChain(BigInteger Id) {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee where id = " + Id);
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapperWithChain(resultSet));
+            }
+            if (employees.isEmpty()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(employees.get(0));
+            }
+        } catch (SQLException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Employee> getEmployeeById(BigInteger Id, int lev) {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee where id = " + Id);
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapper(resultSet, ++lev));
+            }
+            if (employees.isEmpty()) {
+                return Optional.empty();
+            } else {
+                return Optional.of(employees.get(0));
+            }
+        } catch (SQLException e) {
+            return Optional.empty();
+        }
+    }
+
+    public List<Employee> getEmployeeByDepartment(Department department) {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee where department = " + department.getId());
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapper(resultSet,0));
+            }
+            return employees;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public List<Employee> getEmployeeByManager(Employee employee) {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee where manager = " + employee.getId());
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapper(resultSet,0));
+            }
+            return employees;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+
+    public List<Employee> getAllEmployeesWithChain() {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee");
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapperWithChain(resultSet));
+            }
+            return employees;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public List<Employee> getAllEmployees() {
+        try {
+            ResultSet resultSet = getResultSet("select * from employee");
+            List<Employee> employees = new ArrayList<>();
+            while (resultSet.next()) {
+                employees.add(employeeRowMapper(resultSet,0));
+            }
+            return employees;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public List<Department> getAllDeps() {
+        try {
+            ResultSet resultSet = getResultSet("select * from department ");
+            List<Department> departments = new ArrayList<>();
+            while (resultSet.next()) {
+                departments.add(departmentRowMapper(resultSet));
+            }
+            return departments;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+
     public EmployeeService employeeService() {
         return new EmployeeService() {
             @Override
             public List<Employee> getAllSortByHireDate(Paging paging) {
-                List<Employee> cur = Eml;
-
+                List<Employee> cur = getAllEmployees();
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
@@ -192,7 +231,7 @@ public class ServiceFactory {
 
             @Override
             public List<Employee> getAllSortByLastname(Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getAllEmployees();
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
@@ -204,7 +243,7 @@ public class ServiceFactory {
 
             @Override
             public List<Employee> getAllSortBySalary(Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getAllEmployees();
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
@@ -216,7 +255,7 @@ public class ServiceFactory {
 
             @Override
             public List<Employee> getAllSortByDepartmentNameAndLastname(Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getAllEmployees();
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
@@ -242,76 +281,76 @@ public class ServiceFactory {
 
             @Override
             public List<Employee> getByDepartmentSortByHireDate(Department department, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByDepartment(department);
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
                         return o1.getHired().compareTo(o2.getHired());
                     }
                 });
-                return getEmployees2(department, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public List<Employee> getByDepartmentSortBySalary(Department department, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByDepartment(department);
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
                         return o1.getSalary().compareTo(o2.getSalary());
                     }
                 });
-                return getEmployees2(department, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public List<Employee> getByDepartmentSortByLastname(Department department, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByDepartment(department);
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
                         return o1.getFullName().getLastName().compareTo(o2.getFullName().getLastName());
                     }
                 });
-                return getEmployees2(department, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public List<Employee> getByManagerSortByLastname(Employee manager, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByManager(manager);
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
                         return o1.getFullName().getLastName().compareTo(o2.getFullName().getLastName());
                     }
                 });
-                return getEmployees(manager, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public List<Employee> getByManagerSortByHireDate(Employee manager, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByManager(manager);
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
                         return o1.getHired().compareTo(o2.getHired());
                     }
                 });
-                return getEmployees(manager, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public List<Employee> getByManagerSortBySalary(Employee manager, Paging paging) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getEmployeeByManager(manager);
                 cur.sort((o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
-                return getEmployees(manager, paging, cur);
+                return getPaging(cur, paging);
             }
 
             @Override
             public Employee getWithDepartmentAndFullManagerChain(Employee employee) {
-                List<Employee> cur = EmlForChain;
-                for (Employee e : cur){
-                    if (e.getId().equals(employee.getId())){
+                List<Employee> cur = getAllEmployeesWithChain();
+                for (Employee e : cur) {
+                    if (e.getId().equals(employee.getId())) {
                         return e;
                     }
                 }
@@ -320,7 +359,7 @@ public class ServiceFactory {
 
             @Override
             public Employee getTopNthBySalaryByDepartment(int salaryRank, Department department) {
-                List<Employee> cur = Eml;
+                List<Employee> cur = getAllEmployees();
                 cur.sort(new Comparator<Employee>() {
                     @Override
                     public int compare(Employee o1, Employee o2) {
@@ -336,26 +375,6 @@ public class ServiceFactory {
                 return qwe.get(salaryRank - 1);
             }
         };
-    }
-
-    private List<Employee> getEmployees2(Department department, Paging paging, List<Employee> cur) {
-        List<Employee> qwe = new ArrayList<>();
-        for (Employee e : cur) {
-            if (e.getDepartment() != null && e.getDepartment().getId().equals(department.getId())) {
-                qwe.add(e);
-            }
-        }
-        return getPaging(qwe, paging);
-    }
-
-    private List<Employee> getEmployees(Employee manager, Paging paging, List<Employee> cur) {
-        List<Employee> qwe = new ArrayList<>();
-        for (Employee e : cur) {
-            if (e.getManager() != null && e.getManager().getId().equals(manager.getId())) {
-                qwe.add(e);
-            }
-        }
-        return getPaging(qwe, paging);
     }
 
 }
